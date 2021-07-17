575f37eeaae14c4be56254a9edeb5c6037f3cac9
jianshen92 <jianshen92@gmail.com>
2021-07-17 17:26:54 +0800

    some formatting

--- src/views/spyfall/SpyfallGame.vue
+++ src/views/spyfall/SpyfallGame.vue
@@ -5,7 +5,7 @@
       Time has ran out. Game Ends.
     </div>
     <div v-else class="title text-center">
-      <CountdownTimer @timeout="onTimeout" :endTime="endTime"/>
+      <CountdownTimer @timeout="onTimeout" :endTime="endTime" />
     </div>
     <v-progress-circular
       indeterminate
@@ -13,19 +13,23 @@
       v-if="isLoading"
     ></v-progress-circular>
     <div v-else>
-      <div class="subtitle-1 font-weight-bold text-center" v-if="isSpy">You are a Spy</div>
-      <div class="subtitle-1 font-weight-bold text-center" v-else>{{location}}</div>
+      <div class="subtitle-1 font-weight-bold text-center" v-if="isSpy">
+        You are a Spy
+      </div>
+      <div class="subtitle-1 font-weight-bold text-center" v-else>
+        {{ location }}
+      </div>
 
       <div class="title font-weight-light text-center">Locations</div>
       <v-row no-gutters>
-          <v-col
-            v-for="location in locationList"
-            v-bind:key="location"
-            cols="12"
-            sm="6"
-          >
-            <SpyfallCard :location="location"/>
-          </v-col>
+        <v-col
+          v-for="location in locationList"
+          v-bind:key="location"
+          cols="12"
+          sm="6"
+        >
+          <SpyfallCard :location="location" />
+        </v-col>
       </v-row>
     </div>
     <v-btn text large color="primary" @click="endGame">End Game</v-btn>
@@ -33,63 +37,63 @@
 </template>
 
 <script>
-  import {mapState, mapGetters, mapMutations} from 'vuex';
-  import CountdownTimer from '@/components/common/CountdownTimer';
-  import SpyfallCard from '@/components/spyfall/SpyfallCard'
+import { mapState, mapGetters, mapMutations } from "vuex";
+import CountdownTimer from "@/components/common/CountdownTimer";
+import SpyfallCard from "@/components/spyfall/SpyfallCard";
 
-  export default {
-    name: "spyfall-game",
-    components: {CountdownTimer, SpyfallCard},
-    data: function() {
-      return {
-          locationList: [],
-          isSpy: false,
-          location: null,
-          startTime: null,
-          endTime: null,
-          gameDurationMinutes: null,
-          isLoading: true,
-          isGameEnd: false,
-      };
+export default {
+  name: "spyfall-game",
+  components: { CountdownTimer, SpyfallCard },
+  data: function () {
+    return {
+      locationList: [],
+      isSpy: false,
+      location: null,
+      startTime: null,
+      endTime: null,
+      gameDurationMinutes: null,
+      isLoading: true,
+      isGameEnd: false,
+    };
+  },
+  computed: {
+    ...mapState(["lobbyId", "username", "lobbyPlayers"]),
+  },
+  created: function () {
+    const params = {
+      username: this.username,
+      lobbyId: this.lobbyId,
+    };
+    this.$socket.emit("spyfall_game_info", params);
+  },
+  sockets: {
+    spyfall_game_info(message) {
+      const playerRole = message.player_roles[this.username].role;
+      this.locationList = message.location_list;
+      this.location = message.location;
+      this.startTime = message.game_start_time;
+      this.endTime = message.game_end_time;
+      this.gameDurationMinutes = message.game_time_minutes;
+      this.isSpy = playerRole == "SPY";
+      this.isLoading = false;
     },
-    computed: {
-      ...mapState(['lobbyId', 'username','lobbyPlayers'])
+    spyfall_end_game() {
+      this.end_game();
+      this.$router.push({ name: "SpyfallLobby" });
     },
-    created: function() {
+  },
+  methods: {
+    ...mapMutations(["end_game"]),
+    onTimeout() {
+      this.isGameEnd = true;
+    },
+    endGame() {
       const params = {
         username: this.username,
-        lobbyId: this.lobbyId
+        lobbyId: this.lobbyId,
       };
-      this.$socket.emit('spyfall_game_info', params);
+      this.$socket.emit("spyfall_end_game", params);
     },
-    sockets: {
-      spyfall_game_info(message) {
-        const playerRole = message.player_roles[this.username].role;
-        this.locationList = message.location_list;
-        this.location = message.location;
-        this.startTime = message.game_start_time;
-        this.endTime = message.game_end_time;
-        this.gameDurationMinutes = message.game_time_minutes;
-        this.isSpy = (playerRole == "SPY");
-        this.isLoading = false;
-      },
-      spyfall_end_game(){
-        this.end_game();
-        this.$router.push({name: "SpyfallLobby"});
-      }
-    },
-    methods: {
-      ...mapMutations(["end_game"]),
-      onTimeout(){
-          this.isGameEnd = true
-      },
-      endGame(){
-        const params = {
-          username: this.username,
-          lobbyId: this.lobbyId
-        };
-        this.$socket.emit('spyfall_end_game', params);
-      }
-    }
-  };
+  },
+};
 </script>
--- src/views/spyfall/SpyfallHome.vue
+++ src/views/spyfall/SpyfallHome.vue
@@ -1,85 +1,91 @@
 <template>
   <v-container d-flex flex-column justify-center align-center fluid>
     <h1>Spyfall</h1>
-    <v-text-field label="Username" v-model="formUsername" required></v-text-field>
+    <v-text-field
+      label="Username"
+      v-model="formUsername"
+      required
+    ></v-text-field>
     <v-btn text large color="primary" @click="createLobby">Create Game</v-btn>
-    <v-text-field label="Lobby ID" v-model="joinLobbyId" required></v-text-field>
+    <v-text-field
+      label="Lobby ID"
+      v-model="joinLobbyId"
+      required
+    ></v-text-field>
     <v-btn text large color="primary" @click="joinLobby">Join Game</v-btn>
   </v-container>
 </template>
 
 <script>
-  import {mapState, mapGetters, mapMutations, mapActions} from 'vuex';
+import { mapState, mapGetters, mapMutations, mapActions } from "vuex";
 
-  export default {
-    name: "spyfall-home",
-    data() {
-      return {
-        joinLobbyId:''
+export default {
+  name: "spyfall-home",
+  data() {
+    return {
+      joinLobbyId: "",
+    };
+  },
+  mounted: function () {
+    console.log("hello");
+    this.setGame({ gameName: "SPYFALL" });
+  },
+  computed: {
+    ...mapState(["lobbyId", "username", "gameName"]),
+    room_id() {
+      return this.room_num.toUpperCase();
+    },
+    formUsername: {
+      get() {
+        return this.username;
+      },
+      set(value) {
+        this.set_username(value);
+      },
+    },
+  },
+  watch: {
+    username(newValue) {
+      console.log("username has changed to " + newValue);
+    },
+    lobbyId(lobbyId) {
+      if (this.lobbyId != "") {
+        console.log("going to lobby " + lobbyId);
+        this.$router.push({ name: "SpyfallLobby" });
+      }
+    },
+  },
+  methods: {
+    ...mapActions(["setGame"]),
+    ...mapMutations(["set_username", "set_room"]),
+    createLobby() {
+      console.log("create lobby");
+      console.log(this.gameName);
+      const params = {
+        gameName: this.gameName,
+        username: this.username,
       };
+      this.$socket.emit("create_lobby", params);
     },
-    mounted: function () {
-      console.log("hello")
-      this.setGame({gameName: "SPYFALL"});
+    joinLobby() {
+      console.log("join lobby " + this.joinLobbyId);
+      const params = {
+        gameName: this.gameName,
+        username: this.username,
+        joinLobbyId: this.joinLobbyId,
+      };
+      this.$socket.emit("join_lobby", params);
     },
-    computed: {
-      ...mapState(['lobbyId', 'username', 'gameName']),
-      room_id() {
-        return this.room_num.toUpperCase();
-      },
-      formUsername: {
-        get () {
-          return this.username
-        },
-        set (value) {
-          this.set_username(value)
-        }
+    joinGame() {
+      // this.set_username(this.username);
+      this.showInputError = false;
+      if (this.room_num) {
+        this.set_room(this.room_id);
+        this.$router.push({ name: "Player", params: { room: this.room_id } });
+      } else {
+        this.showInputError = true;
       }
     },
-    watch: {
-      username(newValue){
-        console.log("username has changed to " + newValue)
-      },
-      lobbyId(lobbyId){
-        if (this.lobbyId != ""){
-          console.log("going to lobby " + lobbyId);
-          this.$router.push({ name: 'SpyfallLobby'});
-        }
-      }
-    },
-    methods: {
-      ...mapActions([
-        'setGame',
-      ]),
-      ...mapMutations(["set_username", "set_room"]),
-      createLobby(){
-        console.log("create lobby")
-        console.log(this.gameName)
-        const params = {
-          gameName: this.gameName,
-          username: this.username
-        };
-        this.$socket.emit('create_lobby', params);
-      },
-      joinLobby(){
-        console.log("join lobby " + this.joinLobbyId);
-        const params = {
-          gameName: this.gameName,
-          username: this.username,
-          joinLobbyId: this.joinLobbyId
-        };
-        this.$socket.emit('join_lobby', params)
-      },
-      joinGame() {
-        // this.set_username(this.username);
-        this.showInputError = false;
-        if (this.room_num) {
-          this.set_room(this.room_id);
-          this.$router.push({name: "Player", params: {room: this.room_id}});
-        } else {
-          this.showInputError = true;
-        }
-      }
-    }
-  };
+  },
+};
 </script>
